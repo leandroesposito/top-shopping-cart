9e800b66904c4b50baf6bc4c65ec7fb60f7e8b37
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-09 20:14:11 -0300

    Add search element on header

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,9 +1,12 @@
+import Search from "../search/Search";
+
 function Header({ cartCount }) {
   return (
     <header>
       <div className="title">
         <h1>IShop</h1>
       </div>
+      <Search />
     </header>
   );
 }
