3d7aecdddafbf40ebe71bc8d5766ce4b2aef65c2
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-09 20:54:41 -0300

    Add user typing test

--- src/components/search/Search.test.jsx
+++ src/components/search/Search.test.jsx
@@ -1,10 +1,32 @@
 import { describe, expect, it, vi } from "vitest";
 import { screen, render } from "@testing-library/react";
 import Search from "./Search.jsx";
+import userEvent from "@testing-library/user-event";
 
 describe("Search component", () => {
   it("render element", () => {
     render(<Search />);
     expect(screen.getByRole("search")).toBeInTheDocument();
+  });
+
+  it("call onChange when typing", async () => {
+    const onChange = vi.fn();
+    const user = userEvent.setup();
+    render(<Search onChange={onChange} />);
+
+    const input = screen.getByRole("search");
+
+    await user.type(input, "pants");
+
+    expect(input).toHaveValue("pants");
+
+    expect(onChange).toHaveBeenCalled();
+  });
+
+  it("not call onChange when user didn't type", async () => {
+    const onChange = vi.fn();
+    render(<Search onChange={onChange} />);
+
+    expect(onChange).not.toHaveBeenCalled();
   });
 });
