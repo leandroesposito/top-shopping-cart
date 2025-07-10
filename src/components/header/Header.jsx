d509ce79dca6f97651ac05569ebbd44b102fcc72
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-10 19:16:44 -0300

    Complete header structure

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,12 +1,22 @@
 import Search from "../search/Search";
+import Navbar from "../navbar/Navbar";
+import CartIcon from "../cartIcon/CartIcon";
 
 function Header({ cartCount }) {
   return (
     <header>
-      <div className="title">
-        <h1>IShop</h1>
+      <div className="left">
+        <Navbar />
       </div>
-      <Search />
+      <div className="middle">
+        <div className="title">
+          <h1>SCP</h1>
+        </div>
+      </div>
+      <div className="right">
+        <Search />
+        <CartIcon cartCount={cartCount} />
+      </div>
     </header>
   );
 }
