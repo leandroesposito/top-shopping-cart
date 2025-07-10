4115a7f67e6ad766c048ab07c797deecdc39e85a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-10 19:17:33 -0300

    Add outlet to show childrens

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,13 @@
 import "./App.css";
 import Header from "./components/header/Header";
-
+import { Outlet } from "react-router-dom";
 function App() {
   return (
     <div className="body">
       <Header />
+      <main>
+        <Outlet />
+      </main>
     </div>
   );
 }
