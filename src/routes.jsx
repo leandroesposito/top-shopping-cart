7c7ab52efd6a6068ee1010c658ad744bc21bf98a
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-10 19:17:20 -0300

    Add Home children

--- src/routes.jsx
+++ src/routes.jsx
@@ -1,11 +1,17 @@
 import App from "./App";
 import ErrorPage from "./ErrorPage";
-
+import Home from "./components/home/Home";
 const routes = [
   {
     path: "/",
     element: <App />,
     errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true,
+        element: <Home />,
+      },
+    ],
   },
 ];
 
