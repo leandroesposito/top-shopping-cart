0a927b7a0f6bbdcd1ab0544e67b5d36f273a08f1
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-10 19:17:05 -0300

    Create Home component

---
+++ src/components/home/Home.jsx
@@ -0,0 +1,10 @@
+function Home() {
+  return (
+    <div className="home">
+      <div className="title">SCP</div>
+      <div className="description">Shopping Cart Project</div>
+    </div>
+  );
+}
+
+export default Home;
