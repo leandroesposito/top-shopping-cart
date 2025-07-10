d849bca36e0128b28deed4fa678a5abea73476a1
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-10 19:16:29 -0300

    Wrap Header component inside router
    Prevent useContext error when using Link components
    inside navbar when no router
    exists

--- src/components/header/Header.test.jsx
+++ src/components/header/Header.test.jsx
@@ -1,10 +1,15 @@
 import { describe, expect, it } from "vitest";
 import { render, screen } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
 import Header from "./Header";
 
 describe("Header component", () => {
   it("render element", () => {
-    render(<Header />);
-    expect(screen.getByText("IShop")).toBeInTheDocument();
+    render(
+      <MemoryRouter>
+        <Header />
+      </MemoryRouter>
+    );
+    expect(screen.getByText("SCP")).toBeInTheDocument();
   });
 });
