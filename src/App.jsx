9bab8646eae6fca9e002dfb83877220ff94fa9b0
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-14 20:07:46 -0300

    Load store items data on app launch

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,16 @@
 import "./App.css";
 import Header from "./components/header/Header";
 import { Outlet } from "react-router-dom";
+import useData from "./Data";
+
 function App() {
+  const { data, loading, error } = useData();
+
   return (
     <div className="body">
       <Header />
       <main>
-        <Outlet />
+        <Outlet context={{ data, loading, error }} />
       </main>
     </div>
   );
