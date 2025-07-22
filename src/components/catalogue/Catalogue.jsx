e44b3c0e70f65cfb94eed288ebdfcf1b040dc646
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-22 17:07:52 -0300

    Import and use style

--- src/components/catalogue/Catalogue.jsx
+++ src/components/catalogue/Catalogue.jsx
@@ -1,12 +1,13 @@
 import ItemCard from "../itemCard/ItemCard";
 import { useOutletContext, useParams } from "react-router-dom";
+import styles from "./Catalogue.module.css";
 
 function Catalogue() {
   const { data, loading, error, addToCart } = useOutletContext();
   const { category } = useParams();
 
   return (
-    <div className="catalogue">
+    <div className={styles.catalogue}>
       {Object.values(data)
         .filter((item) => !category || item.category === category)
         .map((item) => (
