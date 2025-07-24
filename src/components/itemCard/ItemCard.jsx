aeee58b34b2bb63084e44fc5e5f70083bff20bb3
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-24 18:02:35 -0300

    Wrap item image with a link tag

--- src/components/itemCard/ItemCard.jsx
+++ src/components/itemCard/ItemCard.jsx
@@ -8,22 +8,24 @@ function ItemCard({ itemData, addToCart }) {
 
   return (
     <div className="item-card">
-      <div className="item-header">
-        <div className="image-container">
-          <div className={styles.image}>
-            <img src={itemData.image} alt={itemData.title} />
+      <Link to={`/product/${itemData.id}`}>
+        <div className="item-header">
+          <div className="image-container">
+            <div className={styles.image}>
+              <img src={itemData.image} alt={itemData.title} />
+            </div>
+          </div>
+          <div className="cart-button-container">
+            <button
+              className="cart"
+              aria-label="Add to cart"
+              onClick={onAddToCart}
+            >
+              cart-icon
+            </button>
           </div>
         </div>
-        <div className="cart-button-container">
-          <button
-            className="cart"
-            aria-label="Add to cart"
-            onClick={onAddToCart}
-          >
-            cart-icon
-          </button>
-        </div>
-      </div>
+      </Link>
       <div className="item-body">
         <div className="rating">
           <div className="icon-container">
