ed1ae9884cc009fdf96c59ccd5c1952254a61149
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-23 21:04:21 -0300

    Use css module

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,19 +1,20 @@
 import Search from "../search/Search";
 import Navbar from "../navbar/Navbar";
 import CartIcon from "../cartIcon/CartIcon";
+import styles from "./Header.module.css";
 
 function Header({ getTotalItems }) {
   return (
     <header>
-      <div className="left">
+      <div className={styles.left}>
         <Navbar />
       </div>
-      <div className="middle">
-        <div className="title">
+      <div className={styles.middle}>
+        <div className={styles.title}>
           <h1>SCP</h1>
         </div>
       </div>
-      <div className="right">
+      <div className={styles.right}>
         <Search />
         <CartIcon cartCount={getTotalItems()} />
       </div>
