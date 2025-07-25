a99c5a571e7902059669c0df16190d25224a9cad
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-24 21:15:47 -0300

    Wrap page title with Link tag and use style classes

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import Search from "../search/Search";
 import Navbar from "../navbar/Navbar";
 import CartIcon from "../cartIcon/CartIcon";
@@ -5,18 +6,22 @@ import styles from "./Header.module.css";
 
 function Header({ getTotalItems }) {
   return (
-    <header>
-      <div className={styles.left}>
-        <Navbar />
-      </div>
-      <div className={styles.middle}>
-        <div className={styles.title}>
-          <h1>SCP</h1>
+    <header className={styles.siteHeader}>
+      <div>
+        <div className={styles.left}>
+          <Navbar />
         </div>
-      </div>
-      <div className={styles.right}>
-        <Search />
-        <CartIcon cartCount={getTotalItems()} />
+        <div className={styles.middle}>
+          <Link to="/">
+            <div className={styles.title}>
+              <h1>SCP</h1>
+            </div>
+          </Link>
+        </div>
+        <div className={styles.right}>
+          <Search />
+          <CartIcon cartCount={getTotalItems()} />
+        </div>
       </div>
     </header>
   );
