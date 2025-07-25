80b4daeffb07df0afffa95258f6d9a0470cbdd5d
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-24 21:17:36 -0300

    Wrap cart icon with a link to cart

--- src/components/cartIcon/CartIcon.jsx
+++ src/components/cartIcon/CartIcon.jsx
@@ -1,18 +1,21 @@
+import { Link } from "react-router-dom";
 import icon from "../../assets/icons/cart-shopping.svg";
 import styles from "./CartIcon.module.css";
 
 function CartIcon({ cartCount }) {
   return (
-    <div className={styles.cartCount}>
-      <div className="iconContainer">
-        <div className={styles.icon}>
-          <img src={icon} alt="cart" />
+    <Link to="/cart">
+      <div className={styles.cartCount}>
+        <div className="iconContainer">
+          <div className={styles.icon}>
+            <img src={icon} alt="cart" />
+          </div>
         </div>
+        {cartCount > 1 && (
+          <div className={styles.number}>{cartCount ?? null}</div>
+        )}
       </div>
-      {cartCount > 1 && (
-        <div className={styles.number}>{cartCount ?? null}</div>
-      )}
-    </div>
+    </Link>
   );
 }
 
