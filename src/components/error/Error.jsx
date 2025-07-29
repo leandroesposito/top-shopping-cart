5a0a7c3526ff299b3fa073037730958c99765a3c
leandroesposito <71948697+leandroesposito@users.noreply.github.com>
2025-07-29 15:53:05 -0300

    Create Error component

---
+++ src/components/error/Error.jsx
@@ -0,0 +1,12 @@
+import styles from "./Error.module.css";
+
+function Error(error) {
+  return (
+    <div className={styles.error}>
+      Error...
+      <div className={styles.message}>{error.statusText || error.message}</div>
+    </div>
+  );
+}
+
+export default Error;
